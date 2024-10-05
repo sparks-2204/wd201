68abff581b0cf276de8cbd6d99ce87ee7855dd5e
Sai Puneeth Balaji <saipuneethbalaji@gmail.com>
2024-10-05 15:37:39 +0530

    Changes made to hello_world

--- hello_world.js
+++ hello_world.js
@@ -2,4 +2,12 @@ const hello = () =>{
     console.log("Hello World");
 }
 
+let nums = [1,2,3,4,5];
+let newArray = nums.map((element)=>{
+    let temp = Math.floor(Math.random()*100);
+    return element+temp;
+});
+
+console.log(newArray);
+
 hello();
